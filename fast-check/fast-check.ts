fe3f51949d0cbfe57d80da34adbeb3f4e8670f46
longdevbf <longtda5k48gtb@gmail.com>
2025-09-17 19:26:07 +0700

    push:

--- fast-check/fast-check.ts
+++ fast-check/fast-check.ts
@@ -1,6 +1,7 @@
 import * as fc from "https://esm.sh/fast-check@3.1.1";
 
 Deno.test("Property test: Pay to address with random amounts", async () => {
+
   const { Lucid, Emulator, Blockfrost } = await import("https://deno.land/x/lucid/mod.ts");
   
   const seedPhrase = "illness tomato organ credit hybrid path slight bomb allow media credit virtual uncle blast type very certain join feed repeat elbow place aim oblige";
@@ -27,6 +28,7 @@ Deno.test("Property test: Pay to address with random amounts", async () => {
       lucid.selectWalletFromSeed(ACCOUNT_0.seedPhrase);
 
       const recipient = "addr_test1qqhey3pqmmfr45d33f3nj0enwsmswhqkhtm3pkm2nzatqyvxj3cwzvwg9fn3mx4h2vc6tt84ch55kdcp04eeeqgdnd0s6wrjxx";
+
       const tx = await lucid.newTx()
         .payToAddress(recipient, { lovelace: amount })
         .complete();
@@ -34,7 +36,8 @@ Deno.test("Property test: Pay to address with random amounts", async () => {
       const txHash = await emulator.submitTx(signedTx.toString());
       console.log("Transaction hash:", txHash);
       return true; 
+
     }),
-    {numRuns: 5}
+    {numRuns: 50}
   );
 });
